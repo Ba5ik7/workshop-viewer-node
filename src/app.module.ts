430018017d24c87518039e0a40925c4376526289
Wesley Dusell <dusellfam@gmail.com>
2022-06-17 10:06:59 -0400

    Was able to connect after creating another user other an admin.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,6 @@ import { NavigationModule } from './modules/navigation/navigation.module';
 import { WorkshopModule } from './modules/workshop/workshop.module';
 
 import * as mongodbPw from '../mongodbPw.json';
-// const mongodbPw =
-// {
-//   "user": "wesley",
-//   "pw": "Kelly!one57`",
-//   "ip": "143.198.23.3",
-//   "port": "27017"
-// };
-
-console.log(`mongodb://${mongodbPw.user}:${mongodbPw.pw}@${mongodbPw.ip}:${mongodbPw.port}/workshop-viewer`);
-
 
 @Module({
   imports: [
