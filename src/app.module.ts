6b9d2c5173f8fd241de684d62f0f0a85c994ffcb
Wesley DuSell <dusellfam@gmail.com>
2022-06-16 21:21:54 -0400

    Testing mongodb connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,29 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import mongodbPw from '../mongodbPw.json';
+
+
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NavigationModule } from './modules/navigation/navigation.module';
 import { WorkshopModule } from './modules/workshop/workshop.module';
 
+import * as mongodbPw from '../mongodbPw.json';
+// const mongodbPw =
+// {
+//   "user": "wesley",
+//   "pw": "Kelly!one57`",
+//   "ip": "143.198.23.3",
+//   "port": "27017"
+// };
+
+console.log(`mongodb://${mongodbPw.user}:${mongodbPw.pw}@${mongodbPw.ip}:${mongodbPw.port}/workshop-viewer`);
+
+
 @Module({
   imports: [
-    MongooseModule.forRoot(`mongodb://${mongodbPw.user}:${mongodbPw.pw}@${mongodbPw.ip}/workshop-viewer:${mongodbPw.port}`),
+    MongooseModule.forRoot(`mongodb://${mongodbPw.user}:${mongodbPw.pw}@${mongodbPw.ip}:${mongodbPw.port}/workshop-viewer`),
     NavigationModule,
     WorkshopModule
   ],
