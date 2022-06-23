cbb2ce87cf2d9b26436116d26b81699c25649c84
Wesley DuSell <dusellfam@gmail.com>
2022-06-22 23:00:36 -0400

    Lint

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { IUser } from './interfaces/user.interface';
