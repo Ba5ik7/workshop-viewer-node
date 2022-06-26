31af8dff9156c6f22fffe9d8365456948f33d72b
Wesley DuSell <dusellfam@gmail.com>
2022-06-26 18:12:36 -0400

    Send back token and users id to be refence last in work cycle.

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -33,7 +33,8 @@ export class AuthService {
   async login(user: IUser): Promise<{ [key: string]: string }>  {
     const payload = { email: user.email, sub: user._id };
     return {
-      access_token: this.jwtService.sign(payload),
+      uesrId: user._id,
+      accessToken: this.jwtService.sign(payload),
     };
   }
 
