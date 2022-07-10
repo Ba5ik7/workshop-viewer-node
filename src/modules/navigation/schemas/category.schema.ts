2fed492f4c64312c80ae60b3d2f4b097b3452cb3
Wesley DuSell <dusellfam@gmail.com>
2022-07-10 12:49:13 -0400

    Spine case the name to set id when creating a new category.

--- src/modules/navigation/schemas/category.schema.ts
+++ src/modules/navigation/schemas/category.schema.ts
@@ -44,3 +44,15 @@ export class Category {
 }
 
 export const CategorySchema = SchemaFactory.createForClass(Category);
+
+CategorySchema.pre('save', async function() {
+  if(this.isNew) {
+    this.id = spinalCase(this.name);
+  }
+});
+
+function spinalCase(str) {
+  return str.replace(/^[\W_]+|[\W_]+$|([\W_]+)/g, function ($0, $1) {
+              return $1 ? "-" : "";
+         }).replace(/([a-z])(?=[A-Z])/g, '$1-').toLowerCase();
+}
