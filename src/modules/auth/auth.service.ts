a935190b843596770d53671ed5df271da66b2f5b
Wesley DuSell <dusellfam@gmail.com>
2022-10-17 21:29:02 -0400

    Prettier

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/mongoose';
@@ -9,17 +10,16 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  
   constructor(
     @InjectModel(User.name) private userModel: Model<UserDocument>,
     private jwtService: JwtService,
-    private configService: ConfigService
+    private configService: ConfigService,
   ) {}
-  
+
   async createAccount(user): Promise<IUser> {
     return await this.userModel.create(user);
   }
-  
+
   async removeAccount(user): Promise<IUser> {
     return await this.userModel.remove(user);
   }
@@ -35,37 +35,41 @@ export class AuthService {
     return null;
   }
 
-  async login(user: IUser): Promise<{ [key: string]: string }>  {
+  async login(user: IUser): Promise<{ [key: string]: string }> {
     const payload = { email: user.email, sub: user._id };
-    
+
     const tokenOptions = {
       secret: this.configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
-      expiresIn: this.configService.get<string>('JWT_ACCESS_TOKEN_EXPIRATION_TIME')
+      expiresIn: this.configService.get<string>(
+        'JWT_ACCESS_TOKEN_EXPIRATION_TIME',
+      ),
     };
     const accessToken = this.jwtService.sign(payload);
 
     const refreshTokenOptions = {
       secret: this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET'),
-      expiresIn: this.configService.get<string>('JWT_REFRESH_TOKEN_EXPIRATION_TIME')
+      expiresIn: this.configService.get<string>(
+        'JWT_REFRESH_TOKEN_EXPIRATION_TIME',
+      ),
     };
-    const refreshAccessToken = this.jwtService.sign(payload, refreshTokenOptions);
-    
+    const refreshAccessToken = this.jwtService.sign(
+      payload,
+      refreshTokenOptions,
+    );
+
     return {
       _id: user._id,
       accessToken,
-      refreshAccessToken
+      refreshAccessToken,
     };
   }
 
-  logout() {
-  }
+  logout() {}
 
-  refresh() {
-  }
+  refresh() {}
 
   async setCurrentRefreshToken(refreshToken: string, _id: string) {
     const hashRefreshToken = await bcrypt.hash(refreshToken, 10);
     await this.userModel.updateOne({ _id }, { refreshToken: hashRefreshToken });
   }
- 
 }
