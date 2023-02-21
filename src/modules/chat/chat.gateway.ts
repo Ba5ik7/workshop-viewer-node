d5632d278e7da004268f5cfa2faa01c95982a285
Wesley DuSell <dusellfam@gmail.com>
2023-02-21 11:27:45 -0500

    Inject Chat service into the chat gateway.

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -1,8 +1,11 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
+import { ChatService } from './chat.service';
 
 @WebSocketGateway({ path: '/chat', namespace: 'chat', cors: true })
 export class ChatGateway {
+  constructor(private chatService: ChatService) {}
+
   @SubscribeMessage('message')
   handleMessage(client: Socket, user: string): string {
     console.log({
