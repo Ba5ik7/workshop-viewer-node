32ddb3e6605a738399eb64a6879577b334dfacc6
Wesley DuSell <dusellfam@gmail.com>
2023-02-21 11:08:15 -0500

    Setup chat room and messages inferfaces.

--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Message {
+  user: string;
+  content: string;
+}
+
+export interface ChatRoom {
+  users: string[];
+  messages: Message[];
+}
+
 @Injectable()
 export class ChatService {}
