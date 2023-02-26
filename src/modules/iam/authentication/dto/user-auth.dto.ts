ef1983f4745226555d845653de273953f65f5ec7
Wesley DuSell <dusellfam@gmail.com>
2023-02-26 02:36:40 -0500

    More Refactor

--- src/modules/iam/authentication/dto/user-auth.dto.ts
+++ src/modules/iam/authentication/dto/user-auth.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, Matches } from 'class-validator';
 export class UserAuthDto {
-  @IsEmail({ message: 'Invalid email' })
+  @IsEmail({}, { message: 'Invalid email' })
   email: string;
 
   @Matches(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/, {
